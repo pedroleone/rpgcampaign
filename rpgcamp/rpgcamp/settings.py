282bc29276b28d9ad51a66e3e108f44a0300aae5
Pedro Leone <pedroleone@gmail.com>
2017-04-13 09:31:40 -0300

    Login template changes

--- rpgcamp/rpgcamp/settings.py
+++ rpgcamp/rpgcamp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
     
     'campaign.apps.CampaignConfig',    
     'users',
@@ -128,3 +129,6 @@ MEDIA_ROOT = '/media/'
 MEDIA_URL = os.path.join(BASE_DIR, "media/")
 
 LOGIN_REDIRECT_URL = '/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
