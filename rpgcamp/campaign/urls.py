e4c485108628ba41741f29b058b223fec747037f
Pedro Leone <pedroleone@gmail.com>
2017-04-15 00:47:56 -0300

    Style changes

--- rpgcamp/campaign/urls.py
+++ rpgcamp/campaign/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^new_campaign/$', views.create_campaign, name='create_campaign'),
     url(r'^c/(?P<slug>[\w-]+)/$', views.view_campaign, name='view_campaign'),
     url(r'^$', views.index, name='index'),
-    
-    
+        
 ]
