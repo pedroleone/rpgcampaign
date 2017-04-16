e83bea30befbb68bb80005fe25573b724d2f497c
Pedro Leone <pedroleone@gmail.com>
2017-04-16 12:01:03 -0300

    Add player form redone

--- rpgcamp/campaign/urls.py
+++ rpgcamp/campaign/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^new_campaign/$', views.create_campaign, name='create_campaign'),
     url(r'^c/(?P<slug>[\w-]+)/$', views.view_campaign, name='view_campaign'),
-    url(r'^c/(?P<slug>[\w-]+)/add_player/$', views.add_player, name='add_player'),
+    url(r'^c/(?P<slug>[\w-]+)/players/$', views.players, name='players'),
     url(r'^$', views.index, name='index'),
         
 ]
--- rpgcamp/campaign/views.py
+++ rpgcamp/campaign/views.py
@@ -46,7 +46,7 @@ def view_campaign(request, slug):
 
 from django.contrib.auth.models import User
 @login_required(login_url='/login/')
-def add_player(request, slug): 
+def players(request, slug): 
     context = { 'campaign_list': get_campaigns(request) }
     campaign = get_object_or_404(Campaign, slug=slug)
     permission = CampaignUser.objects.filter(campaign=campaign, user=request.user, permission=1)
@@ -60,17 +60,33 @@ def add_player(request, slug):
             username = form.cleaned_data['user']
             user = User.objects.filter(username=username)
             if not user:
-                context['message'] = "Usuario "+ username +" nao encontrado"
+                alert = {
+                    'title': "Erro!",
+                    'type': "danger",
+                    'message': "Usuário <strong>"+ username +"</strong> não encontrado",
+                }
+                context['alert'] = alert
                 form = AddUserForm()
             else:
                 camp_user = CampaignUser.objects.filter(campaign=campaign, user=user[0])
                 if not camp_user:
                     player = CampaignUser(campaign=campaign, user=user[0],permission=2)
                     player.save()
-                    context['message'] = "Usuario "+ username +" cadastrado!"
+                    alert = {
+                        'title': "Sucesso!",
+                        'type': "success",
+                        'message': "Usuário <strong>"+ username +"</strong> cadastrado",
+                    }
+                    context['alert'] = alert                    
                     form = AddUserForm()
                 else:
-                    context['message'] = "Usuario "+ username +" ja estava cadastrado!"
+                    alert = {
+                        'title': "Aviso!",
+                        'type': "warning",
+                        'message': "Usuário <strong>"+ username +"</strong> já estava cadastrado",
+                    }
+                    context['alert'] = alert    
+
                     form = AddUserForm()
     else:
         form = AddUserForm()
