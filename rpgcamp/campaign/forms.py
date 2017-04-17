7a563653a83d22df8999985b4ef35bb80625ccd0
Pedro Leone <pedroleone@gmail.com>
2017-04-17 16:24:15 -0300

    Formulários alterados para conter mensagens melhores de ajuda a novos usuário

--- rpgcamp/campaign/forms.py
+++ rpgcamp/campaign/forms.py
@@ -5,7 +5,16 @@ class CampaignForm(forms.ModelForm):
     class Meta:
         model = Campaign 
         fields = ['name','system','description','private']
-
+        labels = {
+            'name': 'Nome da Campanha',
+            'system': 'Sistema Utilizado',
+            'description': 'Descrição da Campanha',
+            'private': 'Privado'
+        }
+        help_texts = {
+            'name': 'Escolha com cuidado! O nome da sua campanha irá definir o link para acessar a campanha, e não pode ser alterado no futuro.',
+            'private': 'Deixar sua campanha privada irá fazer com que apenas você e seus jogadores possam visualizar a campanha.'        
+        }
 class AddUserForm(forms.Form):
     user = forms.CharField(label='Nome de Usuário', max_length=100)
 
@@ -14,11 +23,11 @@ class SessionForm(forms.ModelForm):
         model = Session
         fields = ['date', 'notes', 'local']
         labels = {
-            'date': 'Date',
+            'date': 'Data da Sessão',
             'notes': 'Observações',
             'local': 'Local do Jogo',
         }
         help_texts = {
-            'notes': 'Você pode usar <a href="http://daringfireball.net/projects/markdown/">Markdown</a> para formatar o texto.',
-            'local': 'Você pode usar <a href="http://daringfireball.net/projects/markdown/">Markdown</a> para formatar o texto.',
+            'notes': 'Opcional. Você pode usar <a href="http://daringfireball.net/projects/markdown/">Markdown</a> para formatar o texto.',
+            'local': 'Opcional. Você pode usar <a href="http://daringfireball.net/projects/markdown/">Markdown</a> para formatar o texto.',
         }
