8d8e28e3ea2fd48c7b298c4f8ff079b94de01c75
Pedro Leone <pedroleone@gmail.com>
2017-04-17 15:47:15 -0300

    Session edit and delete view

--- rpgcamp/campaign/urls.py
+++ rpgcamp/campaign/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^c/(?P<slug>[\w-]+)/newsession/$', views.new_session, name='new_session'),
     url(r'^c/(?P<slug>[\w-]+)/sessions/$', views.view_sessions, name='view_sessions'),
     url(r'^c/(?P<slug>[\w-]+)/sessions/(?P<session_id>[\w-]+)$', views.view_session, name='view_session'),
+    url(r'^c/(?P<slug>[\w-]+)/sessions/(?P<session_id>[\w-]+)/edit$', views.edit_session, name='edit_session'),
     url(r'^c/(?P<slug>[\w-]+)/players/$', views.players, name='players'),
     url(r'^c/(?P<slug>[\w-]+)/players/delete/$', views.delete_player, name='delete_player'),
     url(r'^$', views.index, name='index'),
--- rpgcamp/campaign/views.py
+++ rpgcamp/campaign/views.py
@@ -12,16 +12,16 @@ def get_campaigns(request):
     if request.user.is_authenticated:
         return CampaignUser.objects.filter(user=request.user)
 
+
 def index(request):
     campaign_list = get_campaigns(request)
     context = { 'campaign_list': campaign_list }
-
     return render(request, 'index.html', context=context)
+
 
 @login_required(login_url='/login/')
 def create_campaign(request):
     context = { 'campaign_list': get_campaigns(request) }
-
     if request.method == "POST":
         form = CampaignForm(request.POST)
         if form.is_valid():
@@ -34,9 +34,9 @@ def create_campaign(request):
     context['form'] = form
     return render(request, 'campaign/create_campaign.html', context)
     
+
 def view_campaign(request, slug):
     context = { 'campaign_list': get_campaigns(request) }
-
     campaign = get_object_or_404(Campaign, slug=slug)
     if campaign.private is True:
         permission = CampaignUser.objects.filter(campaign=campaign, user=request.user)
@@ -48,8 +48,8 @@ def view_campaign(request, slug):
         context['next_session'] = next_session
 
     context['campaign'] = campaign
-
     return render(request, 'campaign.html', context=context)
+
 
 from django.contrib.auth.models import User
 @login_required(login_url='/login/')
@@ -61,9 +61,7 @@ def players(request, slug):
         context['add_user_permission'] = False
     else:
         context['add_user_permission'] = True
-    
     context['campaign'] = campaign
-
     if request.method == "POST":
         form = AddUserForm(request.POST)
         if form.is_valid():
@@ -100,9 +98,9 @@ def players(request, slug):
                     form = AddUserForm()
     else:
         form = AddUserForm()
-    
     context['form'] = form
     return render(request, 'campaign/add_user_campaign.html', context=context)
+
 
 @login_required(login_url='/login/')
 def delete_player(request, slug):
@@ -111,6 +109,7 @@ def delete_player(request, slug):
     obj = CampaignUser.objects.get(user=user_id, campaign=campaign_id)
     obj.delete()
     return HttpResponseRedirect(reverse('players', args=[slug]))
+
 
 @login_required(login_url='/login/')
 def new_session(request, slug):
@@ -123,7 +122,6 @@ def new_session(request, slug):
                               date=form.cleaned_data['date'],
                               local=form.cleaned_data['local'],
                               notes=form.cleaned_data['notes'])
-            
             session.save()
             return HttpResponseRedirect(reverse('view_campaign', args=[campaign.slug])) 
     else:
@@ -131,12 +129,13 @@ def new_session(request, slug):
     context['campaign'] = campaign
     context['form'] = form
     return render(request, 'session/new_session.html', context=context)
-            
+
+
+@login_required(login_url='/login/')            
 def view_sessions(request, slug):
     context = { 'campaign_list': get_campaigns(request) }
     campaign = get_object_or_404(Campaign, slug=slug)
     context['campaign'] = campaign
-
     session = Session.objects.filter(campaign=campaign, date__gte=timezone.now())
     if session:
         next_session = session[0]
@@ -145,22 +144,57 @@ def view_sessions(request, slug):
             future_sessions = session[1:]
             context['future_sessions'] = future_sessions
     context['old_sessions'] = Session.objects.filter(campaign=campaign, date__lt=timezone.now())
-    
-
     session = Session.objects.filter(campaign=campaign, date__gte=timezone.now())
     if session:
         context['next_session'] = next_session
-    
-
     return render(request, 'session/view_sessions.html', context=context)
 
+
+@login_required(login_url='/login/')
 def view_session(request, slug, session_id):
     context = { 'campaign_list': get_campaigns(request) }
     campaign = get_object_or_404(Campaign, slug=slug)
     session = get_object_or_404(Session, id=session_id, campaign=campaign)
     context['campaign'] = campaign
     context['session'] = session
-
-
     return render(request, 'session/view_session.html', context=context)
     
+@login_required(login_url='/login/')
+def delete_session(request, slug):
+    context = { 'campaign_list': get_campaigns(request) }
+    campaign = get_object_or_404(Campaign, slug=slug)
+    session = get_object_or_404(Session, id=session_id, campaign=campaign)
+    context['campaign'] = campaign
+    context['session'] = session
+    return render(request, 'session/view_session.html', context=context)
+
+@login_required(login_url='/login/')
+def edit_session(request, slug, session_id):
+    context = { 'campaign_list': get_campaigns(request) }
+    campaign = get_object_or_404(Campaign, slug=slug)
+    session = get_object_or_404(Session, id=session_id, campaign=campaign)
+    initial_data = {
+        'date': session.date,
+        'local': session.local,
+        'notes': session.notes
+    }
+    form = SessionForm(initial_data)
+    print(request.POST)
+    if request.method == "POST":
+        method = request.POST.get('action')
+        if method == 'delete':
+            session.delete()
+            return HttpResponseRedirect(reverse('view_sessions', args=[campaign.slug])) 
+        form = SessionForm(request.POST)
+        if form.is_valid():
+            session = Session(id=session.id,
+                              campaign=campaign, 
+                              date=form.cleaned_data['date'],
+                              local=form.cleaned_data['local'],
+                              notes=form.cleaned_data['notes'])
+            session.save()
+            return HttpResponseRedirect(reverse('view_sessions', args=[campaign.slug])) 
+
+    context['campaign'] = campaign
+    context['form'] = form
+    return render(request, 'session/edit_session.html', context=context)
