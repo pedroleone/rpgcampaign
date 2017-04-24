39a8571d8d08bd119ee802a522b8f09c4a30ffc2
Pedro Leone <pedroleone@gmail.com>
2017-04-24 11:00:02 -0300

    Timezone changes

--- rpgcamp/forum/models.py
+++ rpgcamp/forum/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from campaign.models import Session, Campaign
+from django.utils import timezone
 
 class Topic(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
